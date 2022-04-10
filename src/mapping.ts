1e90d349e44cc4be70d0393ff1f3dc92cf218645
Sudeep Biswas <sudeepbiswas02@gmail.com>
2022-04-10 16:04:17 +0530

    Add whitelist stats

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt, Bytes, Address, log, ethereum } from "@graphprotocol/graph-ts";
+import { BigInt, Bytes, Address, log, ethereum } from '@graphprotocol/graph-ts';
 import {
   AavegotchiDiamond,
   GotchiLendingAdd,
@@ -8,17 +8,13 @@ import {
   GotchiLendingCancel,
   WhitelistCreated,
   WhitelistUpdated,
-} from "../generated/AavegotchiDiamond/AavegotchiDiamond";
-import { Account, GotchiLending, Whitelist } from "../generated/schema";
+} from '../generated/AavegotchiDiamond/AavegotchiDiamond';
+import { Account, GotchiLending, Whitelist } from '../generated/schema';
 
-const FUD = Address.fromHexString("0x403e967b044d4be25170310157cb1a4bf10bdd0f");
-const FOMO = Address.fromHexString(
-  "0x44a6e0be76e1d9620a7f76588e4509fe4fa8e8c8"
-);
-const ALPHA = Address.fromHexString(
-  "0x6a3e7c3c6ef65ee26975b12293ca1aad7e1daed2"
-);
-const KEK = Address.fromHexString("0x42e5e06ef5b90fe15f853f59299fc96259209c5c");
+const FUD = Address.fromHexString('0x403e967b044d4be25170310157cb1a4bf10bdd0f');
+const FOMO = Address.fromHexString('0x44a6e0be76e1d9620a7f76588e4509fe4fa8e8c8');
+const ALPHA = Address.fromHexString('0x6a3e7c3c6ef65ee26975b12293ca1aad7e1daed2');
+const KEK = Address.fromHexString('0x42e5e06ef5b90fe15f853f59299fc96259209c5c');
 
 export function handleGotchiLendingAdd(event: GotchiLendingAdd): void {
   let lending = GotchiLending.load(event.params.listingId.toString());
@@ -46,9 +42,7 @@ export function handleGotchiLendingExecute(event: GotchiLendingExecute): void {
   }
 
   let contract = AavegotchiDiamond.bind(event.address);
-  let response = contract.try_getGotchiLendingListingInfo(
-    event.params.listingId
-  );
+  let response = contract.try_getGotchiLendingListingInfo(event.params.listingId);
   if (!response.reverted) {
     let listingResult = response.value.value0;
     createNewUser(listingResult.borrower.toHex());
@@ -61,35 +55,16 @@ export function handleGotchiLendingExecute(event: GotchiLendingExecute): void {
 }
 
 export function handleGotchiLendingClaim(event: GotchiLendingClaim): void {
-  log.info("Handle gotchi lending triggered ", []);
-  let lending = GotchiLending.load(event.params.listingId.toString());
-  // Lending will always exists on Lending claim event trigger
-  if (lending) {
-    let tokens = event.params.tokenAddresses;
-    let amounts = event.params.amounts;
-    for (let index = 0; index < tokens.length; index++) {
-      const token = tokens[index];
-      const amount = amounts[index];
-      if (token.equals(FUD)) {
-        lending.claimedFUD = lending.claimedFUD.plus(amount);
-        lending.fudEquivalent = lending.fudEquivalent.plus(amount);
-      } else if (token.equals(FOMO)) {
-        lending.claimedFOMO = lending.claimedFOMO.plus(amount);
-        lending.fudEquivalent = lending.fudEquivalent.plus(amount.times(BigInt.fromI32(2)));
-      } else if (token.equals(ALPHA)) {
-        lending.claimedALPHA = lending.claimedALPHA.plus(amount);
-        lending.fudEquivalent = lending.fudEquivalent.plus(amount.times(BigInt.fromI32(4)));
-      } else if (token.equals(KEK)) {
-        lending.claimedKEK = lending.claimedKEK.plus(amount);
-        lending.fudEquivalent = lending.fudEquivalent.plus(amount.times(BigInt.fromI32(10)));
-      }
-      log.info("Claimed {} amount of token {}", [
-        amount.toString(),
-        token.toHexString(),
-      ]);
-      lending.totalAlchemica = lending.totalAlchemica.plus(amount);
-      lending.save();
-    }
+  log.info('Handle gotchi lending triggered ', []);
+
+  let tokens = event.params.tokenAddresses;
+  let amounts = event.params.amounts;
+
+  updateGotchiLendingAmounts(event.params.listingId, tokens, amounts);
+
+  let whitelistId = getWhitelistIdFromListing(event.params.listingId, event.address);
+  if (whitelistId !== BigInt.zero()) {
+    updateWhitelistLendingAmounts(event.params.listingId, tokens, amounts);
   }
 }
 
@@ -110,15 +85,12 @@ export function handleWhitelistUpdated(event: WhitelistUpdated): void {
   createOrUpdateWhitelist(event.params.whitelistId, event);
 }
 
-function createNewGotchiLending(
-  listingId: BigInt,
-  eventAddress: Address
-): GotchiLending {
+function createNewGotchiLending(listingId: BigInt, eventAddress: Address): GotchiLending {
   let lending = new GotchiLending(listingId.toString());
   let contract = AavegotchiDiamond.bind(eventAddress);
   let response = contract.try_getGotchiLendingListingInfo(listingId);
   if (response.reverted) {
-    return new GotchiLending("0");
+    return new GotchiLending('0');
   }
 
   let listingResult = response.value.value0;
@@ -137,7 +109,7 @@ function createNewGotchiLending(
   lending.splitBorrower = listingResult.revenueSplit[1];
   lending.splitOther = listingResult.revenueSplit[2];
 
-  lending.whitelist = listingResult.whitelistId.toString()
+  lending.whitelist = listingResult.whitelistId.toString();
 
   createNewUser(listingResult.lender.toHex());
   createNewUser(listingResult.borrower.toHex());
@@ -170,10 +142,7 @@ function createNewUser(userAddress: string): void {
   }
 }
 
-export function createOrUpdateWhitelist(
-  id: BigInt,
-  event: ethereum.Event
-): Whitelist | null {
+export function createOrUpdateWhitelist(id: BigInt, event: ethereum.Event): Whitelist | null {
   let contract = AavegotchiDiamond.bind(event.address);
   let response = contract.try_getWhitelist(id);
 
@@ -193,8 +162,72 @@ export function createOrUpdateWhitelist(
     whitelist.name = name;
   }
 
-  whitelist.members = members.map<Bytes>((e) => e);
+  whitelist.members = members.map<Bytes>(e => e);
 
   whitelist.save();
   return whitelist;
+}
+
+export function getWhitelistIdFromListing(listingId: BigInt, contractAddress: Address): BigInt | null {
+  let contract = AavegotchiDiamond.bind(contractAddress);
+
+  let response = contract.try_getGotchiLendingListingInfo(listingId);
+  if (!response.reverted) {
+    let listingResult = response.value.value0;
+    return listingResult.whitelistId;
+  }
+  return BigInt.zero();
+}
+
+export function updateGotchiLendingAmounts(listingId: BigInt, tokens: Address[], amounts: BigInt[]): void {
+  let lending = GotchiLending.load(listingId.toString());
+  // Lending will always exists on Lending claim event trigger
+  if (lending) {
+    for (let index = 0; index < tokens.length; index++) {
+      const token = tokens[index];
+      const amount = amounts[index];
+      if (token.equals(FUD)) {
+        lending.claimedFUD = lending.claimedFUD.plus(amount);
+        lending.fudEquivalent = lending.fudEquivalent.plus(amount);
+      } else if (token.equals(FOMO)) {
+        lending.claimedFOMO = lending.claimedFOMO.plus(amount);
+        lending.fudEquivalent = lending.fudEquivalent.plus(amount.times(BigInt.fromI32(2)));
+      } else if (token.equals(ALPHA)) {
+        lending.claimedALPHA = lending.claimedALPHA.plus(amount);
+        lending.fudEquivalent = lending.fudEquivalent.plus(amount.times(BigInt.fromI32(4)));
+      } else if (token.equals(KEK)) {
+        lending.claimedKEK = lending.claimedKEK.plus(amount);
+        lending.fudEquivalent = lending.fudEquivalent.plus(amount.times(BigInt.fromI32(10)));
+      }
+      log.info('Claimed {} amount of token {}', [amount.toString(), token.toHexString()]);
+      lending.totalAlchemica = lending.totalAlchemica.plus(amount);
+      lending.save();
+    }
+  }
+}
+
+export function updateWhitelistLendingAmounts(whitelistId: BigInt, tokens: Address[], amounts: BigInt[]): void {
+  let whitelist = Whitelist.load(whitelistId.toString());
+  if (whitelist) {
+    for (let index = 0; index < tokens.length; index++) {
+      const token = tokens[index];
+      const amount = amounts[index];
+      if (token.equals(FUD)) {
+        whitelist.claimedFUD = whitelist.claimedFUD.plus(amount);
+        whitelist.fudEquivalent = whitelist.fudEquivalent.plus(amount);
+      } else if (token.equals(FOMO)) {
+        whitelist.claimedFOMO = whitelist.claimedFOMO.plus(amount);
+        whitelist.fudEquivalent = whitelist.fudEquivalent.plus(amount.times(BigInt.fromI32(2)));
+      } else if (token.equals(ALPHA)) {
+        whitelist.claimedALPHA = whitelist.claimedALPHA.plus(amount);
+        whitelist.fudEquivalent = whitelist.fudEquivalent.plus(amount.times(BigInt.fromI32(4)));
+      } else if (token.equals(KEK)) {
+        whitelist.claimedKEK = whitelist.claimedKEK.plus(amount);
+        whitelist.fudEquivalent = whitelist.fudEquivalent.plus(amount.times(BigInt.fromI32(10)));
+      }
+      log.info('Claimed {} amount of token {}', [amount.toString(), token.toHexString()]);
+      whitelist.totalAlchemica = whitelist.totalAlchemica.plus(amount);
+      whitelist.save();
+    }
+  }
 }
